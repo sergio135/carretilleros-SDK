0165f5e39f102e71cc45460291b0bb52e0c23a1a
Sergio Garcia Garcia <sgarcia@cupagroup.com>
2017-01-23 18:35:58 +0100

    Arreglar repeticion de envento

--- app.js
+++ app.js
@@ -5,30 +5,36 @@ const {webFrame} = require('electron')
 // Operatividad en la pagina cargada
 const webview = document.getElementById('sap');
 webview.addEventListener('dom-ready', () => {
-  
     //webview.openDevTools();
     //Codigo insertado en la web externa
     webview.insertCSS(`
-        
-    `);
-    webview.executeJavaScript();
 
+    `);
+    webview.executeJavaScript(`
+
+    `);
+    //webview.send(channel)
+});
+
+
+
+window.onload = () => {
     //Evento que recoje el valor de cada click
-    for (let i of document.querySelectorAll('.area-key .box-key .button')) {
+    let boton = document.querySelectorAll('.button');
+    for (let i of boton) {
         i.addEventListener('click', (e) => {
             webview.insertText(i.innerHTML);
         });
     }
-});
 
-// Mostrar los eventos de consola en la consola del software
-webview.addEventListener('console-message', (e) => {
-    //console.log('webview:'+ e.message);
-});
+    // Mostrar los eventos de consola en la consola del software
+    webview.addEventListener('console-message', (e) => {
+        //console.log('webview:'+ e.message);
+    });
 
-// Abrir cerrar teclado
-document.querySelector('#icon-box').addEventListener('click', () => {
-    document.querySelector('#keybottom').classList.toggle('active');
-    document.querySelector('#webviewbox').classList.toggle('active');
-});
-
+    // Abrir cerrar teclado
+    document.querySelector('#icon-box').addEventListener('click', () => {
+        document.querySelector('#keybottom').classList.toggle('active');
+        document.querySelector('#webviewbox').classList.toggle('active');
+    });
+}
